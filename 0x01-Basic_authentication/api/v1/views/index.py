319077dff12e521f331a56baaa247bdbbcb2e918
Natnael-Niguse <natiart07@gmail.com>
2024-02-13 09:29:00 +0000

    alx

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def abort_401() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - test unauthorized endpoint
+    """
+    abort(401)
